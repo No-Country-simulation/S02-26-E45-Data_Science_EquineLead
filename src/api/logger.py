21f084a417e294f28ae6094b2509feeb615d0c7b
aletbm <alexanderdaniel_rios@hotmail.com>
2026-03-07 16:26:32 -0300

    Add Latency monitoring

--- src/api/logger.py
+++ src/api/logger.py
@@ -1,10 +1,18 @@
+import json
 import logging
+
+
+class JSONFormatter(logging.Formatter):
+    def format(self, record):
+        if isinstance(record.msg, dict):
+            return json.dumps(record.msg)
+        return json.dumps({"message": record.getMessage()})
 
 
 def setup_logger():
     logger = logging.getLogger("equinelead")
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter("%(message)s"))
+    handler.setFormatter(JSONFormatter())
     logger.addHandler(handler)
     logger.setLevel(logging.INFO)
     return logger
